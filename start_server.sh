3695392891c7b252bb6f55136364262996ea2cd9
Afonso Raposo <afonsocraposo@gmail.com>
2022-01-06 14:27:10 +0000

    Corrected docker stuff

--- start_server.sh
+++ start_server.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Create .env.tmp file
-echo -e "ISSUER=${ISSUER}\nSECRET_KEY=${SECRET_KEY}\nCLIENT_ID=${CLIENT_ID}\nCLIENT_SECRET=${CLIENT_SECRET}" > .env.tmp
-
-# Load variables
-(test -f .env || touch .env) ; if [[ $(diff .env .env.tmp | wc -l) -ne 0 ]] ; then echo "Importing new ENV variables" ; mv .env.tmp .env ; pub run build_runner clean ; pub run build_runner build --delete-conflicting-outputs ; fi
-
-# Start HTTP serve# Start HTTP serverr
-echo "Starting HTTP server"
-/usr/bin/dart bin/auth_api.dart
+# Start HTTP serverr
+echo "Starting HTTP resources API server"
+dart bin/resources_api.dart
