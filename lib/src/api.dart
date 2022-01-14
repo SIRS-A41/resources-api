f78f56a9c93a4ac2f5055de056c2448578b92d23
Afonso Raposo <robot@MacBook-Pro-de-Afonso.local>
2022-01-14 17:39:02 +0000

    create project

--- lib/src/api.dart
+++ lib/src/api.dart
@@ -50,6 +50,37 @@ class Api {
       }
     });
 
+    router.post('/create', (Request req) async {
+      final userId = req.context['userId'] as String;
+      final jsonData = await req.readAsString();
+
+      if (jsonData.isEmpty) {
+        return Response(HttpStatus.badRequest,
+            body: 'Provide a project name {name: <project-name>}');
+      }
+
+      try {
+        final data = json.decode(jsonData) as Map<String, dynamic>;
+        if (!data.containsKey('name')) {
+          return Response(HttpStatus.badRequest,
+              body: 'Provide a project name {name: <project-name>}');
+        }
+        final name = data['name'];
+
+        await mongo.createProject(userId, name);
+
+        return Response.ok(
+          'Successfully set public key',
+        );
+      } on FormatException {
+        return Response(HttpStatus.badRequest,
+            body: 'Data is not a valid JSON.');
+      } catch (e) {
+        print(e);
+        return Response.internalServerError();
+      }
+    });
+
     final handler =
         Pipeline().addMiddleware(checkAuthorization()).addHandler(router);
 
--- lib/src/mongo.dart
+++ lib/src/mongo.dart
@@ -5,16 +5,19 @@ typedef MongoFunction = Function(Db db);
 class Mongo {
   late Db test;
   late Db db;
+  late Db projects;
   late DbCollection keys;
   late String mongoUrl;
 
   Mongo(this.mongoUrl)
       : test = Db('$mongoUrl/test'),
-        db = Db('$mongoUrl/db');
+        db = Db('$mongoUrl/db'),
+        projects = Db('$mongoUrl/projects');
 
   Future<void> init() async {
     await test.open();
     await db.open();
+    await projects.open();
     print('Connected to database: $mongoUrl');
     keys = test.collection('public-keys');
   }
@@ -36,5 +39,16 @@ class Mongo {
         'key': publicKey,
       });
     }
+  }
+
+  Future<bool> createProject(String userId, String name) async {
+    final userProjects = projects.collection(userId);
+    final result = await userProjects.findOne(where.eq('name', name));
+
+    if (result == null) return false;
+
+    await userProjects.insertOne({'name': name, 'created_at': DateTime.now()});
+
+    return true;
   }
 }
